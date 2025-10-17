3f157dfd81a78b19d146fbfcb69aa57bd224a097
Tanya <tetanamihajluk@MacBook-Air-Tetana.local>
2025-10-17 23:30:35 +0300

    Update header and footer + add auth and cart context providers

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,14 +1,25 @@
+import "./styles/global.css";
 import HeaderClient from "./components/HeaderClient";
 import Footer from "./components/Footer";
-import "./styles/global.css";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+
+export const metadata = {
+  title: "GIGA STORE",
+  description: "GIGA STORE - electronics",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <HeaderClient />
-        {children}
-        <Footer />
+        <AuthProvider>
+          <CartProvider>
+            <HeaderClient />
+            {children}
+            <Footer />
+          </CartProvider>
+        </AuthProvider>
       </body>
     </html>
   );
